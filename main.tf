c36101f5428fddf32daa6f4e453c44d427cfaca4
francosoa <francosoamota@gmail.com>
2022-11-03 11:15:10 -0300

    add_dynamo

--- main.tf
+++ main.tf
@@ -31,3 +31,22 @@ resource "aws_s3_bucket" "dev4" {
     Environment = "Dev"
   }
 }
+
+#Create a database to instance Dev-6
+resource "aws_dynamodb_table" "table-dev6" {
+  provider = "aws.us-east-2"
+  name = "Dynamo-Dev6"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "UserId"
+  range_key = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute{
+    name = "GameTitle"
+    type = "S"
+  }
+}
